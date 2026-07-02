4b98ecf0d20af7e42a5c448f7026638330c28ebb
JoudDahbour <Jouddahbour1@gmail.com>
2026-07-02 00:45:34 -0400

    Added getPlayerGuess function

--- script.js
+++ script.js
@@ -1,3 +1,18 @@
 function generateRandomNumber() {
     return Math.floor(Math.random() * 100) + 1;
 }
+
+function getPlayerGuess() {
+    let guess = prompt("Enter your guess (between 1 and 100):");
+
+    while (guess === null || guess.trim() === "" || isNaN(guess) || guess < 1 || guess > 100) {
+        if (guess === null) {
+            guess = prompt("You can't escape, creator! Enter a number between 1 and 100:");
+        } else {
+            guess = prompt("'" + guess + "' is not valid! Enter a number between 1 and 100:");
+        }
+           
+    }
+
+    return parseInt(guess);
+}
